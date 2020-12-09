3c425a16432123d6b3228ef6fb60bc3c8731cb5e
feitingjun <49221252@qq.com>
2020-12-09 15:38:02 +0800

    新增jsx和es6转换

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('babel', function () {
+  return gulp.src(['src/**/*.jsx', 'src/**/*.js', 'src/**/*.ts', 'src/**/*.tsx']).
+    pipe(babel({
+      plugins: ['transform-react-jsx'],
+      presets: ['@babel/preset-env']
+    })).
+    pipe(gulp.dest('lib/'))
+});
+gulp.task('copy',  function() {
+  return gulp.src('src/**/*.less')
+    .pipe(gulp.dest('lib'))
+});
+gulp.task('default', gulp.parallel('babel', 'copy'));
